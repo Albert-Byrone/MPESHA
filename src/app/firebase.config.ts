973ae402d7b6eecf3d242529f742f9ffaa03e04e
Albert Byrone <albertbyrone1677@gmail.com>
2023-11-20 22:31:23 +0300

    install firebase

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBU8kHKz_SCCJiN-tbY1YxWd7H0kLblGco',
+  authDomain: 'mpesa-975fd.firebaseapp.com',
+  projectId: 'mpesa-975fd',
+  storageBucket: 'mpesa-975fd.appspot.com',
+  messagingSenderId: '173056908245',
+  appId: '1:173056908245:web:0a3a10ddc75251a9f87b5c',
+};
