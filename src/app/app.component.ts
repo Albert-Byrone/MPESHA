8a8ccd9c60d89d91cd8dcf86d05d558831f4532a
Albert Byrone <albertbyrone1677@gmail.com>
2023-11-20 22:30:18 +0300

    create the app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Quickk';
+
+  isDashboard: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isDashboard = this.router.url.includes('dashboard');
+      }
+    });
+  }
 }
